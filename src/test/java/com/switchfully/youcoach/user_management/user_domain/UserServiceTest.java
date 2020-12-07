eca79c24bf3d56e2417dbdd0109c4da1fe7a2527
gsaegom <guillermosaezgomez@gmail.com>
2020-12-07 12:01:15 +0100

    Story 01: Register finished (email testing halfway)

---
+++ src/test/java/com/switchfully/youcoach/user_management/user_domain/UserServiceTest.java
@@ -0,0 +1,12 @@
+package com.switchfully.youcoach.user_management.user_domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void name() {
+    }
+}
