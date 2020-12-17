feb9fa01d64cd499a77fefc665171d1a930f2185
gsaegom <guillermosaezgomez@gmail.com>
2020-12-17 15:42:33 +0100

    Story 09: Added Get request to get all topic names.

---
+++ src/main/java/com/switchfully/youcoach/coach_management/coach_service/coach_dto/GetTopicNameDto.java
@@ -0,0 +1,13 @@
+package com.switchfully.youcoach.coach_management.coach_service.coach_dto;
+
+public class GetTopicNameDto {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
