b5c98b2038471be6563ce5c4f2e9bba57c360645
Hans3910 <hendrickxhans88@gmail.com>
2020-12-18 10:52:24 +0100

    filters

--- src/main/java/com/switchfully/youcoach/coach_management/coach_domain/repository/TopicNameRepository.java
+++ src/main/java/com/switchfully/youcoach/coach_management/coach_domain/repository/TopicNameRepository.java
@@ -4,4 +4,5 @@ import com.switchfully.youcoach.coach_management.coach_domain.entity.TopicName;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TopicNameRepository extends CrudRepository<TopicName, String> {
+
 }
